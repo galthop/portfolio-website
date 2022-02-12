9d9dd9b401433b943c51bdb37bec91b18dc7d09d
galthop <hitenpatel365@gmail.com>
2022-02-12 22:10:22 +0000

    everything completed minus the descriptions

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,18 @@
 const About = () => {
   return (
     <div className='flex justify-center flex-wrap'>
-      <p className='max-w-2xl py-4 px-8 bg-blue-200 shadow-lg rounded-lg my-2 mx-2 flex justify-center mt-4 text-black text-3xl italic'>
-        About: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Laboriosam in perspiciatis officiis? Vero id rem obcaecati facilis
-        quisquam expedita? Ex doloremque dolore adipisci. Sunt numquam, itaque
-        minus eius est iste!
-      </p>
+      <div className='max-w-[80vw] py-4 px-8 bg-blue-200 shadow-lg rounded-lg my-2 mx-2 flex justify-center mt-4 font-sans'>
+        <div>
+          <p className='mt-2 text-black text-xl leading-8'>
+            I'm an aspiring junior, fullstack developer who has just completed
+            the Northcoders coding bootcamp. My technical skillset includes
+            JavaScript, React, Postgres, Tailwind (which I used to build this
+            website!) and more! I am currently studying PhaserJS for a new
+            project, and improving and extending some of my existing projects
+            below.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,19 @@
 const Title = () => {
   return (
     <div>
+      <div className='bg-blue-200 py-3 mb-3 border-blue-400 border-b-4 border-solid'>
+        <h1 className='text-3xl font-bold underline text-center'>
+          Hiten Patel's portfolio website
+        </h1>
+      </div>
       <img
-        className='rounded-full w-[35vw] h-[35vw] object-cover border-blue-400 border-4 border-solid container max-w-[250px] max-h-[250px] mx-auto flex justify-center shadow hover:shadow-2xl hover:border-blue-700'
+        className='rounded-full w-[35vw] h-[35vw] object-cover border-blue-400 border-4 border-solid container max-w-[250px] max-h-[250px] mx-auto flex justify-center'
         src={require('../my_image.jpg')}
         alt='headshot'
       />
-      <h1 className='text-3xl font-bold underline text-center'>Hiten Patel</h1>
+      <h1 className='text-3xl font-bold underline text-center my-3'>
+        About me
+      </h1>
     </div>
   );
 };
